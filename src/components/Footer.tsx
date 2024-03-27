8f2b59a253e00be04ee2b06caa6617443566b350
dsabirbaev <davran9559@gmail.com>
2024-03-28 00:15:16 +0500

    done footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,39 @@
 
-
+import Link from "next/link"
 
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="bg-[#161616] pt-10 pb-4">
+      <div className="container">
+        <div className="h-[110px] flex flex-col justify-between">
+          <ul className="flex items-center justify-between">
+            <li>
+              <Link href="#" className="text-[#FFFFFF80] hover:text-[#eb0000] text-[14px]">Mening kabinetim</Link>
+            </li>
+            <li>
+              <Link href="#" className="text-[#FFFFFF80] hover:text-[#eb0000] text-[14px]">Biz haqimizda</Link>
+            </li>
+            <li>
+              <Link href="#" className="text-[#FFFFFF80] hover:text-[#eb0000] text-[14px]">Aloqa uchun</Link>
+            </li>
+            <li>
+              <Link href="#" className="text-[#FFFFFF80] hover:text-[#eb0000] text-[14px]">Ommaviy oferta</Link>
+            </li>
+          </ul>
+
+          <div className="flex items-center justify-between text-[#FFFFFF80] text-[14px]">
+            <div>
+              2016-2021 - 100K.UZ
+            </div>
+
+            <div>
+              Barcha huquqlar himoyalangan
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
   )
 }
 
