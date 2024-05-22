1d874fce5eed4c54f43688e49476b1cb2bbc4cd0
dsabirbaev <davran9559@gmail.com>
2024-05-22 10:34:05 +0500

    add react mask library

--- src/app/admin/signin/page.tsx
+++ src/app/admin/signin/page.tsx
@@ -28,11 +28,16 @@ const page = () => {
                     <span>kabinetiga kirish</span>
                 </h1>
 
-                <div className="flex flex-col gap-y-3">
+                <div className="flex flex-col gap-y-3 mb-20">
                     <input type="text" placeholder="Telefon raqamingiz" className="bg-[#ECEEF799] outline-none border-none rounded-[16px] text-[20px] h-[64px] px-[10px] w-[414px] placeholder:text-[16px]"/>
                     <label htmlFor="g" className="flex items-center gap-x-1">
                         <input id="g" type="checkbox" className="w-[15px] h-[15px]"/>
-                        <p className="text-[14px]">Men foydalanish shartlari bilan tanishdim</p>
+                        <p className="text-[14px]">Men
+                            <Link href="#" className="ml-2 hover:underline font-medium">
+                                 foydalanish shartlari
+                            </Link>  bilan tanishdim
+                            
+                            </p>
                     </label>
 
                     <button className="bg-[#fc3] text-[18px] text-[#2b2b2b] font-medium p-[16px] rounded-[18px]">
@@ -44,8 +49,18 @@ const page = () => {
                         Shaxsiy parol orqali kirish
                     </button>
                 </div>
+
+                <div className="flex flex-col items-center">
+                    <div className="flex items-center gap-x-4 text-[#2f8af5] text-[14px] mb-[10px]">
+                        <span>Maxfiylik siyosati</span>
+                        <span>Foydalanish shartlari</span>
+                    </div>
+                    <p className="text-[14px]">Copyright © 2020 Barcha huquqlar himoyalangan</p>
+                </div>
             </section>
         </main>
+
+        
     </div>
   )
 }
